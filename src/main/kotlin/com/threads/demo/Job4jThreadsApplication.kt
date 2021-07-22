36b74a8ed98e6c2b25c32c3633fec32651b44c8c
Alexander Kaleganov <alexmur07@mail.ru>
2021-07-23 00:11:54 +0700

    INIT1

---
+++ src/main/kotlin/com/threads/demo/Job4jThreadsApplication.kt
@@ -0,0 +1,11 @@
+package com.threads.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Job4jThreadsApplication
+
+fun main(args: Array<String>) {
+    runApplication<Job4jThreadsApplication>(*args)
+}
---
+++ src/test/kotlin/com/threads/demo/Job4jThreadsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.threads.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Job4jThreadsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
