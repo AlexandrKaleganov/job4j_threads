519330ea58b47c47457cb863344bb376b4281ebd
Alexander Kaleganov <alexmur07@mail.ru>
2021-07-23 00:42:41 +0700

    2

---
+++ src/main/java/com/threads/concurrent/Job4jThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.threads.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jThreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jThreadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/threads/concurrent/Job4jThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threads.concurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jThreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
