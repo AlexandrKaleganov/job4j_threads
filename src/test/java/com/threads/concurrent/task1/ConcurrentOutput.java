4f2af6cbe1c75d081511f58ff5898dfe9fb0c273
Alexander Kaleganov <alexmur07@mail.ru>
2021-07-23 00:46:30 +0700

    1. Запуск нити. Thread#start() [#1016]

---
+++ src/test/java/com/threads/concurrent/task1/ConcurrentOutput.java
@@ -0,0 +1,15 @@
+package com.threads.concurrent.task1;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+@Log4j2
+public class ConcurrentOutput {
+
+    @Test
+    public void testThread() {
+        new Thread(() -> log.info(Thread.currentThread().getName())).start();
+        log.info(Thread.currentThread().getName());
+    }
+
+}
